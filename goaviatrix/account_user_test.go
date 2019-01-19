5a5d087f86a7a6ed95e9c83c70b53148585b722e
Daniel Huenink <daniel.huenink@nelnet.net>
2019-01-19 11:26:15 -0600

    added test for  account_user.go

---
+++ goaviatrix/account_user_test.go
@@ -0,0 +1,55 @@
+package goaviatrix
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	acctUser = `{
+		"return": true,
+		"results": "New user login:api_user1 has been added to account:devtest successfully - Please check email confirmation."
+	  }`
+)
+
+func TestCreateAccountUser(t *testing.T) {
+	tf := "loginRespSuccess.json"
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		if r.Form.Get("action") == "login" {
+			w.Write([]byte(fixture(tf)))
+		}
+		if r.Form.Get("action") == "add_account_user" {
+			assert.Equal(t, "57e098ed708a8", r.Form.Get("CID"))
+			assert.Equal(t, "api_user1", r.Form.Get("username"))
+			assert.Equal(t, "devtest", r.Form.Get("account_name"))
+			assert.Equal(t, "api_user1@test.com", r.Form.Get("email"))
+			assert.Equal(t, "test123!", r.Form.Get("password"))
+			w.Write([]byte(acctUser))
+		}
+	})
+	httpClient, teardown := testingHTTPClient(h)
+	defer teardown()
+
+	client, err := NewClient("testuser", "testing123!", "localhost", SetHTTPClient(httpClient), BaseURL(server.URL+"/v1/api"))
+	if err != nil {
+		fmt.Println("unable to create client")
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, "57e098ed708a8", client.CID)
+
+	user := AccountUser{
+		UserName:    "api_user1",
+		AccountName: "devtest",
+		Email:       "api_user1@test.com",
+		Password:    "test123!",
+	}
+	if err := client.CreateAccountUser(&user); err != nil {
+		fmt.Println("unable to create user account")
+	}
+	assert.Nil(t, err)
+
+}
