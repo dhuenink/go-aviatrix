0f4543b838f594c5f98a4e7399f1892025f19a18
Daniel Huenink <daniel.huenink@nelnet.net>
2019-01-20 08:29:44 -0600

    added tests for account_user.go

--- goaviatrix/account_user_test.go
+++ goaviatrix/account_user_test.go
@@ -8,19 +8,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	acctUser = `{
-		"return": true,
-		"results": "New user login:api_user1 has been added to account:devtest successfully - Please check email confirmation."
-	  }`
-)
-
 func TestCreateAccountUser(t *testing.T) {
-	tf := "loginRespSuccess.json"
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		if r.Form.Get("action") == "login" {
-			w.Write([]byte(fixture(tf)))
+			w.Write([]byte(fixture("loginRespSuccess.json")))
 		}
 		if r.Form.Get("action") == "add_account_user" {
 			assert.Equal(t, "57e098ed708a8", r.Form.Get("CID"))
@@ -28,7 +20,7 @@ func TestCreateAccountUser(t *testing.T) {
 			assert.Equal(t, "devtest", r.Form.Get("account_name"))
 			assert.Equal(t, "api_user1@test.com", r.Form.Get("email"))
 			assert.Equal(t, "test123!", r.Form.Get("password"))
-			w.Write([]byte(acctUser))
+			w.Write([]byte(fixture("createAccountUser.json")))
 		}
 	})
 	httpClient, teardown := testingHTTPClient(h)
@@ -88,4 +80,108 @@ func TestGetAccountUser(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, user.UserName, resp.UserName)
 
+}
+
+func TestUpdateAccountUser(t *testing.T) {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		if r.Form.Get("action") == "login" {
+			w.Write([]byte(fixture("loginRespSuccess.json")))
+		}
+		if r.Form.Get("action") == "edit_account_user" {
+			assert.Equal(t, "57e098ed708a8", r.Form.Get("CID"))
+			assert.Equal(t, "api_user1", r.Form.Get("username"))
+			assert.Equal(t, "devtest", r.Form.Get("account_name"))
+			assert.Equal(t, "account_name", r.Form.Get("what"))
+			w.Write([]byte(fixture("updateAccountUser.json")))
+		}
+	})
+	httpClient, teardown := testingHTTPClient(h)
+	defer teardown()
+
+	client, err := NewClient("testuser", "testing123!", "localhost", SetHTTPClient(httpClient), BaseURL(server.URL+"/v1/api"))
+	if err != nil {
+		fmt.Println("unable to create client")
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, "57e098ed708a8", client.CID)
+
+	user := AccountUserEdit{
+		UserName:    "api_user1",
+		What:        "account_name",
+		AccountName: "devtest",
+	}
+	if err := client.UpdateAccountUserObject(&user); err != nil {
+		fmt.Println("unable to update user account")
+	}
+	assert.Nil(t, err)
+
+}
+
+func TestDeleteAccountUser(t *testing.T) {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		params := r.URL.Query()
+		if r.Form.Get("action") == "login" {
+			w.Write([]byte(fixture("loginRespSuccess.json")))
+		}
+		if len(params) != 0 {
+			assert.Equal(t, "57e098ed708a8", params["CID"][0])
+			assert.Equal(t, "delete_account_user", params["action"][0])
+			w.Write([]byte(fixture("deleteAccountUser.json")))
+		}
+	})
+	httpClient, teardown := testingHTTPClient(h)
+	defer teardown()
+
+	client, err := NewClient("testuser", "testing123!", "localhost", SetHTTPClient(httpClient), BaseURL(server.URL+"/v1/api"))
+	if err != nil {
+		fmt.Println("unable to create client")
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, "57e098ed708a8", client.CID)
+
+	user := AccountUser{
+		UserName: "api_user2",
+	}
+	if err := client.DeleteAccountUser(&user); err != nil {
+		fmt.Printf("error: %v", err)
+	}
+	assert.Nil(t, err)
+
+}
+
+func TestListAccountUser(t *testing.T) {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		params := r.URL.Query()
+		if r.Form.Get("action") == "login" {
+			w.Write([]byte(fixture("loginRespSuccess.json")))
+		}
+		if len(params) != 0 {
+			assert.Equal(t, "57e098ed708a8", params["CID"][0])
+			assert.Equal(t, "list_account_users", params["action"][0])
+			w.Write([]byte(fixture("listAccountUsers.json")))
+		}
+	})
+	httpClient, teardown := testingHTTPClient(h)
+	defer teardown()
+
+	client, err := NewClient("testuser", "testing123!", "localhost", SetHTTPClient(httpClient), BaseURL(server.URL+"/v1/api"))
+	if err != nil {
+		fmt.Println("unable to create client")
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, "57e098ed708a8", client.CID)
+
+	users, err := client.ListAccountUsers()
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+	assert.Equal(t, "user1", (*users)[0].UserName)
+	assert.Equal(t, "acct1", (*users)[0].AccountName)
+	assert.Equal(t, "user1@example.com", (*users)[0].Email)
+	assert.Equal(t, "user2", (*users)[1].UserName)
+	assert.Equal(t, "acct1", (*users)[1].AccountName)
+	assert.Equal(t, "user2@example.com", (*users)[1].Email)
 }
